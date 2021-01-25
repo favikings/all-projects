3bba885121d33ef57db1acc78cadef0ea7a04371
favikings <faviasuainok@gmail.com>
2021-01-25 17:33:42 +0100

    Added interface

---
+++ projects/todoApp/toggle.js
@@ -0,0 +1,11 @@
+const mobileNav = document.querySelector('.mobile-nav');
+const menuBtn = document.querySelector('.menu-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener('click', ()=>{
+    mobileNav.classList.add('hide');
+});
+
+closeBtn.addEventListener('click', ()=>{
+    mobileNav.classList.remove('hide');
+})
